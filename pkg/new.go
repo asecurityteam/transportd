9ee691444c16546cb6202095b9b904b39ac147fa
Kevin Conway <kconway@atlassian.com>
2019-05-02 13:54:02 -0500

    Separate transport and openapi creation
    These functions did not need to be coupled. Move the openapi
    specification parsing to a helper method and have the constructors parse
    before calling in to make a transport.

--- pkg/new.go
+++ pkg/new.go
@@ -12,43 +12,45 @@ import (
 	"github.com/getkin/kin-openapi/openapi3filter"
 )
 
-// this method handles the fact that the runtime and the transport creation
-// both need the parsed openAPI spec but don't both need to parse it.
-func newTransport(ctx context.Context, specification []byte, components ...NewComponent) (http.RoundTripper, *openapi3.Swagger, error) {
+func newSpecification(source []byte) (*openapi3.Swagger, error) {
 	envProcessor := NewEnvProcessor()
-	specification, err := envProcessor.Process(specification)
+	source, err := envProcessor.Process(source)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	loader := openapi3.NewSwaggerLoader()
-	swagger, errYaml := loader.LoadSwaggerFromData(specification)
+	swagger, errYaml := loader.LoadSwaggerFromData(source)
 	var errJSON error
 	if errYaml != nil {
-		swagger, errJSON = loader.LoadSwaggerFromData(specification)
+		swagger, errJSON = loader.LoadSwaggerFromData(source)
 	}
 	if errYaml != nil && errJSON != nil {
-		return nil, nil, errJSON
+		return nil, errJSON
 	}
+	return swagger, nil
+}
+
+func newTransport(ctx context.Context, specification *openapi3.Swagger, components ...NewComponent) (http.RoundTripper, error) {
 	router := openapi3filter.NewRouter()
-	err = router.AddSwagger(swagger)
+	err := router.AddSwagger(specification)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	// Load and configure available backends.
 	var rawBackendConf interface{}
 	var ok bool
-	if rawBackendConf, ok = swagger.Extensions[ExtensionKey]; !ok {
-		return nil, nil, fmt.Errorf("missing backend configuration")
+	if rawBackendConf, ok = specification.Extensions[ExtensionKey]; !ok {
+		return nil, fmt.Errorf("missing backend configuration")
 	}
 	s, err := SourceFromExtension([]byte(rawBackendConf.(json.RawMessage)))
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to parse backend configuration: %s", err.Error())
+		return nil, fmt.Errorf("failed to parse backend configuration: %s", err.Error())
 	}
 	transports, err := NewBaseTransports(ctx, s)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to configure backends: %s", err.Error())
+		return nil, fmt.Errorf("failed to configure backends: %s", err.Error())
 	}
 
 	// Load and configure endpoints.
@@ -57,18 +59,18 @@ func newTransport(ctx context.Context, specification []byte, components ...NewCo
 		Bases:      transports,
 		Components: components,
 	}
-	for path, pathItem := range swagger.Paths {
+	for path, pathItem := range specification.Paths {
 		for method, op := range pathItem.Operations() {
 			if _, ok = op.Extensions[ExtensionKey]; !ok {
-				return nil, nil, fmt.Errorf("missing client configuration for %s.%s", path, method)
+				return nil, fmt.Errorf("missing client configuration for %s.%s", path, method)
 			}
 			opS, opErr := SourceFromExtension([]byte(op.Extensions[ExtensionKey].(json.RawMessage)))
 			if opErr != nil {
-				return nil, nil, fmt.Errorf("failed to parse client configuration for %s.%s: %s", path, method, opErr.Error())
+				return nil, fmt.Errorf("failed to parse client configuration for %s.%s: %s", path, method, opErr.Error())
 			}
 			client, opErr := clientF.New(ctx, opS, path, method)
 			if opErr != nil {
-				return nil, nil, fmt.Errorf("failed client configuration for %s.%s: %s", path, method, opErr.Error())
+				return nil, fmt.Errorf("failed client configuration for %s.%s: %s", path, method, opErr.Error())
 			}
 			reg.Store(ctx, path, method, client)
 		}
@@ -76,20 +78,28 @@ func newTransport(ctx context.Context, specification []byte, components ...NewCo
 	return &ClientTransport{
 		Router:   router,
 		Registry: reg,
-	}, swagger, nil
+	}, nil
 }
 
 // NewTransport constructs a smart HTTP client from the given specification
 // and set of plugins. For running a service, use the New method instead.
 func NewTransport(ctx context.Context, specification []byte, components ...NewComponent) (http.RoundTripper, error) {
-	transport, _, err := newTransport(ctx, specification, components...)
+	spec, err := newSpecification(specification)
+	if err != nil {
+		return nil, err
+	}
+	transport, err := newTransport(ctx, spec, components...)
 	return transport, err
 }
 
 // New generates a configured HTTP runtime. To use as a library, call the
 // NewTransport method instead.
 func New(ctx context.Context, specification []byte, components ...NewComponent) (*runhttp.Runtime, error) {
-	transport, swagger, err := newTransport(ctx, specification, components...)
+	spec, err := newSpecification(specification)
+	if err != nil {
+		return nil, err
+	}
+	transport, err := newTransport(ctx, spec, components...)
 	if err != nil {
 		return nil, err
 	}
@@ -117,7 +127,7 @@ func New(ctx context.Context, specification []byte, components ...NewComponent) 
 	// Load and configure the runtime settings.
 	var rawRuntimeConf interface{}
 	var ok bool
-	if rawRuntimeConf, ok = swagger.Extensions[RuntimeExtensionKey]; !ok {
+	if rawRuntimeConf, ok = spec.Extensions[RuntimeExtensionKey]; !ok {
 		return nil, fmt.Errorf("missing x-runtime configuration")
 	}
 	s, err := RuntimeSourceFromExtension([]byte(rawRuntimeConf.(json.RawMessage)))
