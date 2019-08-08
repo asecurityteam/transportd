66f53539388e54ce48e4508439bba7cef7005150
gabriel case <gcase@atlassian.com>
2019-08-08 10:43:34 -0500

    added comments as per linting feedback

--- pkg/components/ldap_auth.go
+++ pkg/components/ldap_auth.go
@@ -33,25 +33,33 @@ func (r *authValidationTransport) RoundTrip(req *http.Request) (*http.Response, 
 	return r.Wrapped.RoundTrip(req)
 }
 
+
+// AuthConfig is used to configure authorization based on ldap group membership sent in a header
 type AuthConfig struct {
 	AllowedGroups []string `description:"List of ldap groups allowed to access your service"`
 	LdapGroupsHeaderName string `description:"Name of the header that contains the ldap group membership of an incoming request"`
 }
 
+
+// Name of the config root
 func (*AuthConfig) Name() string {
 	return "authconfig"
 }
 
+// AuthConfigComponent is a plugin
 type AuthConfigComponent struct {}
 
+// Auth satisfies the NewComponent signature
 func Auth(_ context.Context, _ string, _ string, _ string) (interface{}, error) {
 	return &AuthConfigComponent{}, nil
 }
 
+// Settings generates a config populated with defaults.
 func (*AuthConfigComponent) Settings() *AuthConfig {
 	return &AuthConfig{}
 }
 
+// New generates the middleware.
 func (*AuthConfigComponent) New(_ context.Context, conf *AuthConfig) (func(tripper http.RoundTripper) http.RoundTripper, error) {
 	return func(wrapped http.RoundTripper) http.RoundTripper {
 		return &authValidationTransport{
