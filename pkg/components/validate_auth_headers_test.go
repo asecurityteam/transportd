a4960728bb19aa2b147284a822ce600e5b62ad77
gabriel case <gcase@atlassian.com>
2019-08-22 14:50:25 -0500

    adding init of new test

--- pkg/components/validate_auth_headers_test.go
+++ pkg/components/validate_auth_headers_test.go
@@ -1,2 +1,24 @@
 package components
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_incomingMatchesAllowed(t *testing.T) {
+	allowedHeader := make(map[string][]string)
+	goodIncomingHeader := make(map[string][]string)
+	badIncomingHeader := make(map[string][]string)
+
+	goodIncomingHeader["LDAP-Groups"] = []string{"sre", "devs"}
+	badIncomingHeader["LDAP-Groups"] = []string{"design"}
+	allowedHeader["LDAP-Groups"] = []string{"sre"}
+
+	result := incomingMatchesAllowed(allowedHeader, goodIncomingHeader)
+	assert.Equal(t, result, true)
+	result = incomingMatchesAllowed(allowedHeader, badIncomingHeader)
+	assert.Equal(t, result, false)
+	badIncomingHeader["LDAP-Groups"] = nil
+	result = incomingMatchesAllowed(allowedHeader, badIncomingHeader)
+	assert.Equal(t, result, false)
+}
