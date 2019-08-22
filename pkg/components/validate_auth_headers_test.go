de965a5a76e05a89bb170fc7ae8c45af67e427e9
gabriel case <gcase@atlassian.com>
2019-08-22 17:21:36 -0500

    added first test

--- pkg/components/validate_auth_headers_test.go
+++ pkg/components/validate_auth_headers_test.go
@@ -1,24 +1,59 @@
 package components
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_incomingMatchesAllowed(t *testing.T) {
-	allowedHeader := make(map[string][]string)
-	goodIncomingHeader := make(map[string][]string)
-	badIncomingHeader := make(map[string][]string)
-
-	goodIncomingHeader["LDAP-Groups"] = []string{"sre", "devs"}
-	badIncomingHeader["LDAP-Groups"] = []string{"design"}
-	allowedHeader["LDAP-Groups"] = []string{"sre"}
-
-	result := incomingMatchesAllowed(allowedHeader, goodIncomingHeader)
-	assert.Equal(t, result, true)
-	result = incomingMatchesAllowed(allowedHeader, badIncomingHeader)
-	assert.Equal(t, result, false)
-	badIncomingHeader["LDAP-Groups"] = nil
-	result = incomingMatchesAllowed(allowedHeader, badIncomingHeader)
-	assert.Equal(t, result, false)
+	defaultAllowedHeaderAndValues := map[string][]string{"LDAP-Groups": {"sre", "devs"}}
+	tests := []struct {
+		name string
+		allowedHeader map[string][]string
+		incomingHeader map[string][]string
+		wantResult bool
+	}{
+		{
+			name: "good incoming header values",
+			allowedHeader: defaultAllowedHeaderAndValues,
+			incomingHeader: map[string][]string{"LDAP-Groups": {"sre", "devs"}},
+			wantResult: true,
+		},
+		{
+			name: "good incoming header values with single required header value",
+			allowedHeader: map[string][]string{"LDAP-Groups": {"devs"}},
+			incomingHeader: map[string][]string{"LDAP-Groups": {"sre", "devs"}},
+			wantResult: true,
+		},
+		{
+			name: "bad incoming header values",
+			allowedHeader: defaultAllowedHeaderAndValues,
+			incomingHeader: map[string][]string{"LDAP-Groups": {"design"}},
+			wantResult: false,
+		},
+		{
+			name: "missing incoming header",
+			allowedHeader: defaultAllowedHeaderAndValues,
+			incomingHeader: map[string][]string{"meh": {"sre", "devs"}},
+			wantResult: false,
+		},
+		{
+			name: "missing single incoming header value",
+			allowedHeader: defaultAllowedHeaderAndValues,
+			incomingHeader: map[string][]string{"LDAP-Groups": {"devs"}},
+			wantResult: false,
+		},
+		{
+			name: "missing incoming header and values",
+			allowedHeader: defaultAllowedHeaderAndValues,
+			incomingHeader: map[string][]string{"": {""}},
+			wantResult: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			if result := incomingMatchesAllowed(tt.allowedHeader, tt.incomingHeader); result != tt.wantResult {
+				t.Errorf("wanted: %t got: %t", tt.wantResult, result)
+			}
+		})
+	}
 }
