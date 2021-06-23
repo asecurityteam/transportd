2f3fee41b4d359630cd7840075eec864f4e81fbf
Alena Khalina <akhalina@atlassian.com>
2021-06-23 11:33:29 -0400

    Remove lib from test, update mod

--- tests/new_test.go
+++ tests/new_test.go
@@ -8,7 +8,6 @@ import (
 
 	transportd "github.com/asecurityteam/transportd/pkg"
 	"github.com/asecurityteam/transportd/pkg/components"
-	packr "github.com/gobuffalo/packr/v2"
 )
 
 type tc struct {
@@ -18,31 +17,35 @@ type tc struct {
 }
 
 func TestNewService(t *testing.T) {
-	data := packr.New("data", "./specs")
+	folder := "./specs/"
 
 	tcs := []tc{
 		{
 			Name:    "success",
-			Spec:    data.Bytes("complete.yaml"),
+			File:    "complete.yaml",
 			WantErr: false,
 		},
 		{
 			Name:    "missing runtime",
-			Spec:    data.Bytes("missingruntime.yaml"),
+			File:    "missingruntime.yaml",
 			WantErr: true,
 		},
 		{
 			Name:    "passthrough enabled",
-			Spec:    data.Bytes("passthroughenabled.yaml"),
+			File:    "passthroughenabled.yaml",
 			WantErr: false,
 		},
 	}
 
 	for _, tt := range tcs {
 		t.Run(tt.Name, func(t *testing.T) {
-			_, err := transportd.New(
+			fileInput, err := ioutil.ReadFile(folder + tt.File)
+			if err != nil {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.WantErr)
+			}
+			_, err = transportd.New(
 				context.Background(),
-				[]byte(tt.Spec),
+				fileInput,
 				components.Defaults...,
 			)
 			if (err != nil) != tt.WantErr {
